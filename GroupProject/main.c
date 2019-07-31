2d2dbcb447286e6df5ac4f31b3ce794dedb9a436
Heon Lee <>
2019-07-31 19:02:36 -0400

    Starting

--- GroupProject/main.c
+++ GroupProject/main.c
@@ -1,10 +1,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-//Testing
+//Read from and Write to a record txt file
+//Add any necessary functions and change return type
+//If successful, return the Heap allocated array pointer to main
+//If failed, return null
+void readFile();
+void writeFile();
+
+//View functions
+void showAll();
+void viewDetails(int);
+
+//Basic functions
+void add();
+void edit();
+void delete();
+
+//Search
+void search();
+
+//Additional functionality
+void filterByGenre();
+
 int main() {
-
+    
     return 0;
 }
+
 
